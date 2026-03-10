06486bd08e5ad6ac002cc9f40d01b19223dd0eaa
fp-torres <felipetorresaraujo@gmail.com>
2026-03-10 12:42:31 -0300

    refactor: remove optional comment from Blob 3 in Background component

--- src/components/layout/Background.jsx
+++ src/components/layout/Background.jsx
@@ -5,7 +5,7 @@ export default function Background() {
       <div className="absolute top-[-10%] left-[-10%] w-[500px] h-[500px] bg-surface/40 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob"></div>
       {/* Blob 2 */}
       <div className="absolute top-[-10%] right-[-10%] w-[500px] h-[500px] bg-slate-800/40 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000"></div>
-      {/* Blob 3 (Opcional, cor primária bem suave) */}
+      {/* Blob 3  */}
       <div className="absolute -bottom-8 left-20 w-[500px] h-[500px] bg-primary/5 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
     </div>
   );
